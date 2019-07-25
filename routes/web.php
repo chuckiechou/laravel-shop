0ae715da75c9b9fdb653eda6566f0ac556bbe518
chuckiechou <zzp208@qq.com>
2019-07-25 23:23:00 +0800

    email

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 Route::get('/index', 'PagesController@index')->name('index');
-Auth::routes();
+Auth::routes(['verify' => true]);
 
