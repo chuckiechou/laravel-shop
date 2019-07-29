fd070dae74d293f56637012131109d8e8a0dc3a3
chuckiechou <zzp208@qq.com>
2019-07-29 23:00:45 +0800

    add address

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 Route::get('/', 'PagesController@root')->name('root');
 Route::get('/index', 'PagesController@index')->name('index');
 Auth::routes(['verify' => true]);
-Route::group(['middleware' => ['auth']], function() {
+Route::group(['middleware' => ['auth']], function () {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
 
